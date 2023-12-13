665ac3f7f9c02bb1146a2682d62eb11f68e5ee77
Andrew Xiong <andrewxiong@csus.edu>
2023-12-13 13:23:03 -0800

    added js

---
+++ Assignment5/fancifymytext.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert("Hello, world!");
+}
+
+function makeBigger() {
+    showAlert(); // Call the function to show the alert
+    document.getElementById("inputText").style.fontSize = "24pt";
+}
